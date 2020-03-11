8004f7c4801f3d5f13c1ec5490a0ed272c3c6b09
ph0513 <61400137+ph0513@users.noreply.github.com>
2020-03-11 10:16:21 +0900

    Update Action.java

--- src/action/Action.java
+++ src/action/Action.java
@@ -2,7 +2,7 @@ package action;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+// Action Interface
 public interface Action {
 	public ActionForward execute(HttpServletRequest req,
 				HttpServletResponse res) throws Exception;
